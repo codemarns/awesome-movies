a3ca8c9c11d7fa996d2f5775a5444d9d7dd6347d
Marnien Cueba <marniencueba94@gmail.com>
2024-02-21 14:47:41 +0800

    Search: border color and icon changes when focus

--- components/Search.js
+++ components/Search.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, TextInput } from "react-native";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import styles from "../styles/App.scss";
 
 export const Search = ({ value, onChangeText }) => {
+  const [focus, setFocus] = useState(false);
+
   return (
     <View style={styles.search}>
       <View style={styles.searchBar}>
-        <MagnifyingGlassIcon style={styles.searchIcon} />
+        <MagnifyingGlassIcon
+          style={styles.searchIcon}
+          color={focus ? "ghostwhite" : "slategray"}
+        />
         <TextInput
           style={styles.searchInput}
           placeholder="Search"
           value={value}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
           onChangeText={onChangeText}
         />
       </View>
